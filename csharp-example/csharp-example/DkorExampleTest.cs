f95acbdb38607c1bd7fd8166ee73f927d6a911ff
Dmitry Korneev <31965644+korneev89@users.noreply.github.com>
2018-02-09 10:13:52 +0300

    renamed classes and tests

--- csharp-example/csharp-example/DkorExampleTest.cs
+++ csharp-example/csharp-example/DkorExampleTest.cs
@@ -7,7 +7,7 @@ using OpenQA.Selenium.Support.UI;
 namespace csharp_example
 {
 	[TestFixture]
-	public class MyFirstTest
+	public class DkorExampleTest
 	{
 		private IWebDriver driver;
 		private WebDriverWait wait;
@@ -20,21 +20,12 @@ namespace csharp_example
 		}
 
 		[Test]
-		public void FirstTest()
+		public void ExampleTest()
 		{
 			driver.Url = "http://www.google.com/";
 			driver.FindElement(By.Name("q")).SendKeys("webdriver");
 			driver.FindElement(By.Name("q")).Submit();
 			wait.Until(ExpectedConditions.TitleIs("webdriver - Google Search"));
-		}
-
-		[Test]
-		public void Lesson2Task3()
-		{
-			driver.Url = "http://localhost:8080/litecart/admin/login.php";
-			driver.FindElement(By.Name("username")).SendKeys("admin");
-			driver.FindElement(By.Name("password")).SendKeys("admin");
-			driver.FindElement(By.Name("login")).Click();
 		}
 
 		[TearDown]
---
+++ csharp-example/csharp-example/DkorLogin.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace csharp_example
+{
+	[TestFixture]
+	public class DkorLogin
+	{
+		private IWebDriver driver;
+		private WebDriverWait wait;
+
+		[SetUp]
+		public void Start()
+		{
+			driver = new ChromeDriver();
+			wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+		}
+
+		[Test]
+		public void Login()
+		{
+			driver.Url = "http://localhost:8080/litecart/admin/login.php";
+			driver.FindElement(By.Name("username")).SendKeys("admin");
+			driver.FindElement(By.Name("password")).SendKeys("admin");
+			driver.FindElement(By.Name("login")).Click();
+		}
+
+		[TearDown]
+		public void Stop()
+		{
+			driver.Quit();
+			driver = null;
+		}
+	}
+}
