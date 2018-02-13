264dd8bc2978863b85ddafee9719a3755f450c2a
Dmitry Korneev <31965644+korneev89@users.noreply.github.com>
2018-02-13 16:09:17 +0300

    Ex8
    check if stickers exist

---
+++ csharp-example/csharp-example/Lesson 4/Ex8.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace csharp_example
+{
+	[TestFixture]
+	public class Ex8 : TestBase
+	{
+		[SetUp]
+		public void Start()
+		{
+			driver = new ChromeDriver();
+			wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+		}
+
+		[Test]
+		public void StickersExist()
+		{
+			driver.Url = "http://localhost:8080/litecart";
+
+			var ducks = driver.FindElements(By.CssSelector(".product.column.shadow.hover-light"));
+			foreach (IWebElement duck in ducks)
+			{
+				Assert.IsTrue( duck.FindElements(By.CssSelector(".sticker")).Count == 1 );
+				//Debug.WriteLine(duck.FindElement(By.CssSelector(".sticker")).Text);
+			}
+		}
+	}
+}
