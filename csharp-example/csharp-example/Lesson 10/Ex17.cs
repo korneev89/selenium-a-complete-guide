ea89ac01595fcd95ff6d890e86d385fc954770df
Dmitry Korneev <31965644+korneev89@users.noreply.github.com>
2018-03-12 11:17:13 +0300

    Ex. 17

---
+++ csharp-example/csharp-example/Lesson 10/Ex17.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Diagnostics;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support.UI;
+
+namespace csharp_example
+{
+	[TestFixture]
+	public class Ex17 : TestBase
+	{
+		[SetUp]
+		public void Start()
+		{
+			ChromeOptions options = new ChromeOptions();
+			options.SetLoggingPreference("performance", LogLevel.All);
+
+			driver = new ChromeDriver(options);
+			wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+		}
+
+		[Test]
+		public void CheckBrowserLog()
+		{
+			LoginAsAdmin();
+			var logTypes = driver.Manage().Logs.AvailableLogTypes;
+			driver.Url = "http://localhost:8080/litecart/admin/?app=catalog&doc=catalog&category_id=1";
+			var itemsCount = driver.FindElements(By.CssSelector("img[style*='margin-left: 32px; width: 16px; height: 16px; vertical-align: bottom;'] + a")).Count;
+
+			for (var i = 0; i < itemsCount; i++)
+			{
+				driver.FindElements(By.
+					CssSelector("img[style*='margin-left: 32px; width: 16px; height: 16px; vertical-align: bottom;'] + a"))[i].
+						Click();
+
+				foreach (var logType in logTypes)
+				{
+					var logsCount = driver.Manage().Logs.GetLog(logType).Count;
+					var infoMessage = String.Concat("log type: ", logType, ", logs count: ", logsCount);
+					if (logType != "performance")
+					{
+						Assert.AreEqual(logsCount,0);
+					}
+					Debug.WriteLine(infoMessage);
+				}
+
+				driver.Url = "http://localhost:8080/litecart/admin/?app=catalog&doc=catalog&category_id=1";
+			}
+
+		}
+	}
+}
